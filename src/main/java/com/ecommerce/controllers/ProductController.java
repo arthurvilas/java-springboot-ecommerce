e47f84b5a5b8dac06fe52131ac9d4bf135b69406
Arthur Vilas <arthurvilas5@gmail.com>
2022-01-15 23:02:22 -0300

    Created product model, get and post methods

--- src/main/java/com/ecommerce/controllers/ProductController.java
+++ src/main/java/com/ecommerce/controllers/ProductController.java
@@ -1,6 +1,17 @@
 package com.ecommerce.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import javax.websocket.server.PathParam;
+
+import com.ecommerce.model.Product;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +19,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/products") // http://localhost:8080/products
 public class ProductController {
 
-    @GetMapping("/") // HTTP GET request
-    public String helloWorld() {
-        return "Hello World!";
+    private List<Product> productsList = new ArrayList<>();
+
+    // Get a product
+    @GetMapping("/{id}")
+    public Product product(@PathVariable("id") Long id) {
+        Optional<Product> foundProduct = productsList.stream().filter(p -> p.getId() == id).findFirst();
+        if (foundProduct.isEmpty()) {
+            return null;
+        }
+        return foundProduct.get();
+    }
+
+    // Create a product
+    @PostMapping("/")
+    public Product product(@RequestBody Product createdProduct) {
+        productsList.add(createdProduct);
+        return createdProduct;
+    }
+
+    // Get all products
+    @GetMapping("/list")
+    public List<Product> list() {
+        return productsList;
     }
 }
---
+++ src/main/java/com/ecommerce/model/Product.java
@@ -0,0 +1,50 @@
+package com.ecommerce.model;
+
+public class Product {
+    private Long id;
+    private String name;
+    private Double price;
+    private String description;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, Double price, String description) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
